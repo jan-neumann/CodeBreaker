9c4643891e30d650f185b5870c75c62b4e93ef72
Jan Neumann <jan.alexander.neumann@gmail.com>
2026-01-14 10:58:17 +0100

    Add pegs

--- CodeBreaker/ContentView.swift
+++ CodeBreaker/ContentView.swift
@@ -8,14 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            pegs([.green, .blue, .yellow, .red])
+            pegs([.green, .red, .yellow, .red])
+            pegs([.green, .blue, .blue, .red])
         }
         .padding()
+        
+    }
+    
+    func pegs(_ colors: Array<Color>) -> some View {
+        HStack {
+            Circle().foregroundStyle(colors[0])
+            Circle().foregroundStyle(colors[1])
+            Circle().foregroundStyle(colors[2])
+            Circle().foregroundStyle(colors[3])
+        }
     }
 }
 
