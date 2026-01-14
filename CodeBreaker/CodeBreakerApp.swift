0438fa50d32992cf5b8965d6e60c76e775ff3826
Jan Neumann <jan.alexander.neumann@gmail.com>
2026-01-14 08:16:16 +0100

    Initial Commit

---
+++ CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by Jan Neumann on 14.01.26.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeBreakerTests/CodeBreakerTests.swift
@@ -0,0 +1,16 @@
+//
+//  CodeBreakerTests.swift
+//  CodeBreakerTests
+//
+//  Created by Jan Neumann on 14.01.26.
+//
+
+import Testing
+
+struct CodeBreakerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
